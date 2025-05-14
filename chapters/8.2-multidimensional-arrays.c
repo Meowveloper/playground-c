c7d2b793d76004d120d15e1e00d88d0517621677
Meowveloper <meowveloper.dev@gmail.com>
2025-05-15 01:41:40 +0700

    just updating

---
+++ chapters/8.2-multidimensional-arrays.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <time.h>
+
+void deal_a_hand_of_cards(void);
+
+int main(void) {
+	deal_a_hand_of_cards();
+	return 0;
+}
+
+void deal_a_hand_of_cards(void) {
+	printf("hello world\n");
+}
+
